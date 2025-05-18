720c0d6b3f34793d5888eefad21b1e5e8fec38f0
Divesh Kshirsagar <diveshkshirsagar42@gmail.com>
2025-05-18 12:30:37 +0530

    removed bug in apiresponse

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -1,5 +1,5 @@
 class ApiResponse {
-    constuctor(statusCode, data, message = "success") {
+    constructor(statusCode, data, message = "success") {
         this.statusCode = statusCode;
         this.data = data;
         this.message = message;
